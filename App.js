0b85ac55bceee285f8feac713f3822a9bd52b312
CooperA02 <141257756+CooperA02@users.noreply.github.com>
2024-01-19 14:04:30 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>This Will be my Incredible To Do App!</Text>
+      <Text>Step 1:</Text>
+      <Text>Step 2:</Text>
+      <Text>Step 3:</Text>
+      <Text>Step 4:</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
